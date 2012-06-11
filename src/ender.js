0f3ab85259596490e7d0c6ef459463d24765cfe5
Rod Vagg <rod@vagg.org>
2012-06-11 13:42:58 +1000

    more verbose $.X method names in ender integration

--- src/ender.js
+++ src/ender.js
@@ -1,4 +1,9 @@
 (function ($) {
-  var c = require('categorizr')
-  $.ender(c)
+  var c = require('categorizr'), o = {}, k
+  for (k in c) {
+    if (Object.hasOwnProperty.call(c, k)) {
+      o[k == 'test' ? 'testUserAgent' : k == 'is' ? 'isDeviceType' : k] = c[k]
+    }
+  }
+  $.ender(o)
 }(ender));
