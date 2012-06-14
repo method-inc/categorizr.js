75e81708f9b669e824afdfe47090fd3af5745334
Dustan Kasten <dustan.kasten@gmail.com>
2012-06-14 14:25:48 -0400

    Remove ender integration file

--- src/ender.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-  var c = require('categorizr'), o = {}, k
-  for (k in c) {
-    if (Object.hasOwnProperty.call(c, k)) {
-      o[k == 'test' ? 'testUserAgent' : k == 'is' ? 'isDeviceType' : k] = c[k]
-    }
-  }
-  $.ender(o)
-}(ender));
